5e46071a934b085194c4503447e4b174f3e5138a
Patrick Marschke <Patrick.m.marschke@gmail.com>
2022-01-12 22:31:49 +0000

    html stuff

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,13 @@
+<div>
+    <form action="">
+        <input type="hidden" name="done">
+        <button aria-label="Mark done/notdone">Done/Notdone</button>
+    </form>
+    <form action="">
+        <input type="text" name="" id="">
+        <button>Save/Todo</button>
+    </form>
+    <form action="">
+        <button>delete</button>
+    </form>
+</div>
